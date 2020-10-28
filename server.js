607dbbf48f6ca3b9cddf66257111a4dca2ff7bde
Alfie Fu <44627885+fualfie@users.noreply.github.com>
2020-10-28 17:00:55 +0800

    update server version

--- server.js
+++ server.js
@@ -1,12 +1,6 @@
 const path = require('path')
 const WebServer = require('@fualfie/easy-server').WebServer
-let server = new WebServer()
-    .public(__dirname + '/public')
-    .route(__dirname + '/routes/api.js')
-    .error(__dirname + '/public/error.html', __dirname + '/public/error.jpg')
-    .upload(path.resolve("./files"))
-    .log(path.resolve("./logs"))
-    .start();
+let server = new WebServer().start().open().log()
 const WebSocket = require('@fualfie/websocket-server').WebSocket
 server.websocket = new WebSocket().start()
 module.exports = server;
